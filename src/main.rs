a30cd93b86f4576fa06c11e22c60e2a377ff0617
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-20 22:23:13 -0500

    first

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use iced::executor;
+use iced::widget::{column, container, horizontal_space, row, text, text_editor};
+use iced::{Application, Command, Element, Length, Settings, Theme};
+
+use std::io;
+use std::path::Path;
+use std::sync::Arc;
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor {
+    content: text_editor::Content,
+    error: Option<io::ErrorKind>,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
+}
+
+impl Application for Editor {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+                error: None,
+            },
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
+                Message::FileOpened,
+            ),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("NSeditor")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+            Message::FileOpened(Ok(content)) => {
+                self.content = text_editor::Content::with(&content);
+            }
+            Message::FileOpened(Err(error)) => {
+                self.error = Some(error);
+            }
+        }
+        Command::none()
+    }
+
+    fn view(&self) -> iced::Element<'_, Message> {
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        let position = {
+            let (line, column) = self.content.cursor_position();
+
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+
+        let status_bar = row![horizontal_space(Length::Fill), position];
+
+        container(column![input, status_bar].spacing(5))
+            .padding(12)
+            .into()
+    }
+    fn theme(&self) -> Theme {
+        Theme::Dark
+    }
+}
+
+async fn pick_file() -> Result<Arc<String>, Error> {
+    let handle = rfd::AsyncFileDialog::new()
+        .set_title("Choose text file...")
+        .pick_file()
+        .await
+        .ok_or(Error::DialogClosed)?;
+
+    load_file(handle.path()).await
+}
+
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, Error> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
+        .map_err(Error::IO)
+}
+#[derive(Debug, Clone)]
+enum Error {
+    DialogClosed,
+    IO(io::ErrorKind),
+}
